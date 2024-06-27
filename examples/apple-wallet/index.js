62eeccbbf1ee4921c31e3a738b0f8ca1286bb65d
George Parapanissios <gparapanissios@Georges-MBP.station>
2024-06-27 12:34:41 +0300

    add apple wallet example

---
+++ examples/apple-wallet/index.js
@@ -0,0 +1,37 @@
+const { PKPass } = require("passkit-generator");
+const fs = require('fs');
+const path = require('path');
+
+const generatePass = async () => {
+  try {
+    // Define paths
+    const certsPath = path.join(__dirname, 'certs');
+    const passFolderPath = path.join(__dirname, 'model', 'boardingPass.pass');
+
+    // Read certificate contents
+    const wwdr = fs.readFileSync(path.join(certsPath, 'WWDR.pem'));
+    const signerCert = fs.readFileSync(path.join(certsPath, 'signerCert.pem'));
+    const signerKey = fs.readFileSync(path.join(certsPath, 'signerKey.pem'));
+    const signerKeyPassphrase = process.env.PASS_PHRASE;
+
+    // Initialize the pass from a model folder
+    const pass = await PKPass.from({
+      model: passFolderPath,
+      certificates: {
+        wwdr,
+        signerCert,
+        signerKey,
+        signerKeyPassphrase
+      }
+    });
+
+    // Generate the buffer .pkpass file
+    const buffer = pass.getAsBuffer();
+    fs.writeFileSync('yourPass.pkpass', buffer);
+    console.log('Pass generated successfully');
+  } catch (err) {
+    console.error('Error generating pass:', err);
+  }
+};
+
+generatePass();
